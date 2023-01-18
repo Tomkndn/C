279d3496968fa05477e24e257886a3a16c8330c4
Kundan Tamsoy <105445488+Tomkndn@users.noreply.github.com>
2023-01-19 00:28:12 +0530

    Delete C code directory

--- C code/addSDdDv.c
+++
@@ -1,8 +0,0 @@
-// Write a Program to perform addition, subtraction, division and
-// multiplication of two numbers given as input by the user.
-#include<stdio.h>
-int main(){
-    float input;
-    scanf("%f",&input);
-    return 0;
-}
